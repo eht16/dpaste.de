bbff327be8c6eed0e476f969147b93620ad95de4
Enrico Tröger <enrico.troeger@uvena.de>
2012-04-15 18:56:56 +0200

    Enable CSRF protection

--- pastebin/conf/settings.py
+++ pastebin/conf/settings.py
@@ -80,9 +80,9 @@ LOGIN_REDIRECT_URL = '/'
 #==============================================================================
 
 MIDDLEWARE_CLASSES = (
-    'pastebin.disable.DisableCSRF',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
