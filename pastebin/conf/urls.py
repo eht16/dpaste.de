2b9797ae125808567cfdcac9292a3d5f58d42aab
Enrico Tröger <enrico.troeger@uvena.de>
2013-03-31 11:20:01 +0200

    Clean imports and add fake redirect for trafficbasedsspsitemap.xml (Bingbot)

--- pastebin/conf/urls.py
+++ pastebin/conf/urls.py
@@ -1,18 +1,20 @@
 # coding: utf-8
 
-from django.conf.urls.defaults import *
-from django.conf import settings
+from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^trafficbasedsspsitemap\.xml$', RedirectView.as_view(url='http://127.0.0.1/')),
+
     url(r'^about/$', 'django.views.generic.simple.direct_to_template', {'template': 'about.html'}, name='about'),
     url(r'^admin/', include(admin.site.urls)),
 
     #~ url(r'^api/(?P<secret_id>[^/]+)/$', snippet_resource),
     url(r'^api/$', 'pastebin.apps.api.views.create'),
 
-    (r'^', include('pastebin.apps.dpaste.urls')),
+    url(r'^', include('pastebin.apps.dpaste.urls')),
 )
